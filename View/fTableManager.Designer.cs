b4de1f7f85b3259885a6c9848165151a9cdb3376
Vietprole <phuvietdn@gmail.com>
2023-05-14 10:52:38 +0700

    Revert "Merge TDV into main"
    This reverts commit d85807529f3d86423a663df832891ec6e9733f43, reversing
    changes made to a4a4c82972dad730731c3567c9ef1e8ee988ae1b.

--- View/fTableManager.Designer.cs
+++ View/fTableManager.Designer.cs
@@ -227,7 +227,6 @@
             // 
             // nmFoodCount
             // 
-            this.nmFoodCount.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.nmFoodCount.Location = new System.Drawing.Point(985, 58);
             this.nmFoodCount.Margin = new System.Windows.Forms.Padding(4);
             this.nmFoodCount.Minimum = new decimal(new int[] {
