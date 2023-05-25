b8c5bca39042e8a2be52f1c00b5e94ee2f971cd4
Vietprole <phuvietdn@gmail.com>
2023-05-25 18:48:13 +0700

    Add ShiftDatagridview

---
+++ DTO/ShiftDatagridview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3CodeDemo.DTO
+{
+    internal class ShiftDatagridview
+    {
+        public string AccountName { get; set; }
+    }
+}
